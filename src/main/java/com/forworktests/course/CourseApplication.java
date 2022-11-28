479150652394e029358de33559eb3a83d4318ad3
Helder Franco <helderfranco2011@gmail.com>
2022-11-27 22:53:15 -0300

    User entity and resource

--- src/main/java/com/forworktests/course/CourseApplication.java
+++ src/main/java/com/forworktests/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.forworktests.course;
+ package com.forworktests.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
