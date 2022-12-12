deff7e8f4429fd45f3726dab1efd7842cce429c6
Helder Franco <helderfranco2011@gmail.com>
2022-12-12 14:17:22 -0300

    Order, Instant, ISO 8601

--- src/main/java/course/entities/User.java
+++ src/main/java/course/entities/User.java
@@ -1,17 +1,18 @@
 package course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @EqualsAndHashCode
-
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -24,5 +25,20 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+	public User(){
+	}
+
+	public User(Long id, String name, String email, String phone, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 
 }
---
+++ src/main/java/course/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package course.repositories;
+
+import course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
