4d5eb197bfce45d02e6693fa5beb6697b084579a
Helder Franco <helderfranco2011@gmail.com>
2023-04-11 11:43:42 -0300

    Exception handling - findById

---
+++ src/main/java/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1l;
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
